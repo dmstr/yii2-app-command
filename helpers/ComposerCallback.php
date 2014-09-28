dab87e67a559808e70aeb9b480e0ba9981f01691
Tobias Munk <schmunk@usrbin.de>
2014-09-28 21:01:54 +0200

    updated application configuration and init hook

--- helpers/ComposerCallback.php
+++ helpers/ComposerCallback.php
@@ -24,12 +24,16 @@ class ComposerCallback
 
     /**
      * Application initialization
+     *
      * @param Event $event
      */
     public static function init(Event $event)
     {
         require(__DIR__ . '/../../../../init');
-        self::yii(['app/init']);
+        self::yii(['app/configure']);
+        // reload possibly changed configurations
+        self::configureYii();
+        self::yii(['migrate']);
         echo "\nScript-hook 'init' completed.\n\n";
     }
 
@@ -48,16 +52,22 @@ class ComposerCallback
         $basePath = __DIR__ . '/../../../../';
         require($basePath . '/vendor/autoload.php');
         require($basePath . '/vendor/yiisoft/yii2/Yii.php');
-        require($basePath . '/common/config/aliases.php');
+        require($basePath . '/common/config/bootstrap.php');
 
+        self::configureYii();
+        self::$isYiiInitialized = true;
+    }
+
+    private static function configureYii()
+    {
+        $basePath     = __DIR__ . '/../../../../';
+        // TODO: support different application directory layouts
         self::$config = \yii\helpers\ArrayHelper::merge(
             require($basePath . '/common/config/main.php'),
             require($basePath . '/common/config/main-local.php'),
             require($basePath . '/console/config/main.php'),
             require($basePath . '/console/config/main-local.php')
         );
-
-        self::$isYiiInitialized = true;
     }
 
     private static function yii($argv)
