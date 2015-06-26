41f2815d94d739caa131125b28585e017ec34c3c
Tobias Munk <schmunk@usrbin.de>
2015-06-26 21:11:50 +0200

    hardened mysql create command, added timeout, retry and pause option

--- controllers/BaseAppController.php
+++ controllers/BaseAppController.php
@@ -24,6 +24,10 @@ class BaseAppController extends Controller
     public $composerExecutables = ['composer.phar', 'composer'];
     public $defaultAction = 'version';
 
+    public $mysqlRetryMaxCount = 20;
+    public $mysqlRetryTimeout = 2;
+    public $mysqlPreSetupPause = 2;
+
     protected $_composerExecutable = null;
 
     public function init()
@@ -31,7 +35,7 @@ class BaseAppController extends Controller
         parent::init();
         foreach ($this->composerExecutables AS $cmd) {
             // TODO: improve check
-            exec($cmd.' 2>&1', $output, $return);
+            exec($cmd . ' 2>&1', $output, $return);
             if ($return == 0) {
                 $this->_composerExecutable = $cmd;
                 return;
@@ -68,51 +72,58 @@ class BaseAppController extends Controller
     public function actionCreateMysqlDb($db = null)
     {
         if ($db === null) {
-            $db            = getenv("DATABASE_DSN_DB");
+            $db = getenv("DATABASE_DSN_DB");
         }
         if (empty($db)) {
             $this->stdout('No database configured, skipping setup.');
             return;
         }
 
-        $root          = getenv("DB_ENV_MYSQL_ROOT_USER")?:'root';
+        $root = getenv("DB_ENV_MYSQL_ROOT_USER") ?: 'root';
         $root_password = getenv("DB_ENV_MYSQL_ROOT_PASSWORD");
         if (empty($root_password)) {
             return;
         }
-        $user          = getenv("DB_ENV_MYSQL_USER");
-        $pass          = getenv("DB_ENV_MYSQL_PASSWORD");
-        $dsn           = getenv("DATABASE_DSN_BASE");
+        $user = getenv("DB_ENV_MYSQL_USER");
+        $pass = getenv("DB_ENV_MYSQL_PASSWORD");
+        $dsn  = getenv("DATABASE_DSN_BASE");
 
-        $this->stdout("Creating database '{$db}' and granting permissions to user '{$user}' on DSN '{$dsn}' with user '{$root}'");
+        $this->stdout(
+            "Checking database connection on DSN '{$dsn}' with user '{$root}'"
+        );
 
         // trying to connect to database with PDO (20 times, interval 1 second)
         try {
-            // retry an operation up to 5 times
-            $dbh = \igorw\retry(20, function () use ($dsn, $root, $root_password) {
-                $this->stdout('.');
-                sleep(1);
-                return new \PDO($dsn, $root, $root_password);
-            });
+            // retry an operation up to 20 times
+            $dbh = \igorw\retry(
+                $this->mysqlRetryMaxCount,
+                function () use ($dsn, $root, $root_password) {
+                    $this->stdout('.');
+                    sleep($this->mysqlRetryTimeout);
+                    return new \PDO($dsn, $root, $root_password);
+                }
+            );
         } catch (FailingTooHardException $e) {
-            $this->stdout("Unable to connect to database: " . $e->getMessage());
+            $this->stdout("\n\nError: Unable to connect to database '" . $e->getMessage(). "''");
             \Yii::$app->end(1);
         }
+        $this->stdout(' [OK]');
 
         // wait one more second to harden startup and create database credentials
-        sleep(1);
+        sleep($this->mysqlPreSetupPause);
+        $this->stdout(
+            "\nCreating database '{$db}' and granting permissions to user '{$user}' on DSN '{$dsn}' with user '{$root}'\n"
+        );
         try {
             $dbh->exec(
                 "CREATE DATABASE IF NOT EXISTS `$db`;
-         GRANT ALL ON `$db`.* TO '$user'@'%' IDENTIFIED BY '$pass';
-         FLUSH PRIVILEGES;"
+                 GRANT ALL ON `$db`.* TO '$user'@'%' IDENTIFIED BY '$pass';
+                 FLUSH PRIVILEGES;"
             )
             or die(print_r($dbh->errorInfo(), true));
         } catch (\PDOException $e) {
             die("DB ERROR: " . $e->getMessage());
         }
-
-        $this->stdout("\nDatabase successfully created.\n");
     }
 
     protected function composer($command)
